bed2af67655ce62b6ce1bd57fb1e82968751b9f3
Cassidy <cassidy0kovell@gmail.com>
2024-02-26 08:43:17 -0800

    added functions to render licenses

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,92 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+} else if (license === 'Apache 2.0') {
+    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+} else if (license === 'GPL 3.0') {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+} else {
+    return '';
+}
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return 'https://opensource.org/licenses/MIT';
+} else if (license === 'Apache 2.0') {
+    return 'https://opensource.org/licenses/Apache-2.0';
+} else if (license === 'GPL 3.0') {
+    return 'https://www.gnu.org/licenses/gpl-3.0';
+} else {
+    return '';
+}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'MIT') {
+    return `
+## License
+
+This project is licensed under the [MIT License](${renderLicenseLink(license)}).
+`;
+} else if (license === 'Apache 2.0') {
+    return `
+## License
+
+This project is licensed under the [Apache License 2.0](${renderLicenseLink(license)}).
+`;
+} else if (license === 'GPL 3.0') {
+    return `
+## License
+
+This project is licensed under the [GPL 3.0 License](${renderLicenseLink(license)}).
+`;
+} else {
+    return '';
+}
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    return `
+# ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, feel free to contact me at ${data.email}.
 `;
 }
 
+
 module.exports = generateMarkdown;
+
