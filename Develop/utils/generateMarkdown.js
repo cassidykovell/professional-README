a4218a21f4b71224674e1abb1ba6913952f85dbe
Cassidy <cassidy0kovell@gmail.com>
2024-02-26 09:52:47 -0800

    fixed some formatting for the README and correctly exported functions needed in index.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,61 +1,68 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === 'MIT') {
-    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-} else if (license === 'Apache 2.0') {
-    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
-} else if (license === 'GPL 3.0') {
-    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
-} else {
-    return '';
-}
+  if (license === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "Apache 2.0") {
+    return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else if (license === "GPL 3.0") {
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } else {
+    return "";
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license === 'MIT') {
-    return 'https://opensource.org/licenses/MIT';
-} else if (license === 'Apache 2.0') {
-    return 'https://opensource.org/licenses/Apache-2.0';
-} else if (license === 'GPL 3.0') {
-    return 'https://www.gnu.org/licenses/gpl-3.0';
-} else {
-    return '';
-}
+  if (license === "MIT") {
+    return "https://opensource.org/licenses/MIT";
+  } else if (license === "Apache 2.0") {
+    return "https://opensource.org/licenses/Apache-2.0";
+  } else if (license === "GPL 3.0") {
+    return "https://www.gnu.org/licenses/gpl-3.0";
+  } else {
+    return "";
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license === 'MIT') {
+  if (license === "MIT") {
     return `
 ## License
 
 This project is licensed under the [MIT License](${renderLicenseLink(license)}).
 `;
-} else if (license === 'Apache 2.0') {
+  } else if (license === "Apache 2.0") {
     return `
 ## License
 
-This project is licensed under the [Apache License 2.0](${renderLicenseLink(license)}).
+This project is licensed under the [Apache License 2.0](${renderLicenseLink(
+      license
+    )}).
 `;
-} else if (license === 'GPL 3.0') {
+  } else if (license === "GPL 3.0") {
     return `
 ## License
 
-This project is licensed under the [GPL 3.0 License](${renderLicenseLink(license)}).
+This project is licensed under the [GPL 3.0 License](${renderLicenseLink(
+      license
+    )}).
 `;
-} else {
-    return '';
-}
+  } else {
+    return "";
+  }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-    return `
+  const licenseBadge = renderLicenseBadge(data.license);
+  return `
 # ${data.title}
+
+${licenseBadge}
 
 ## Description
 ${data.description}
@@ -74,19 +81,30 @@ ${data.installation}
 ## Usage
 ${data.usage}
 
+
+## Credits
+${data.credits}
+
+## License
 ${renderLicenseSection(data.license)}
 
-## Contributing
-${data.contributing}
+## Badges
+
+## Features 
+${data.features}
+
+## How to Contribute
+${data.contribute}
 
 ## Tests
 ${data.tests}
 
 ## Questions
-If you have any questions, feel free to contact me at ${data.email}.
+${data.github}
+${data.email}
+${data.contact}
 `;
 }
 
-
 module.exports = generateMarkdown;
-
+module.exports = renderLicenseBadge;
--- Develop/utils/index.js
+++ Develop/utils/index.js
@@ -1,84 +1,88 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const renderLicenseBadge = require('./generateMarkdown.js');
+const renderLicenseSection = require('./generateMarkdown.js')
 
-const questions = [     
-            {
-                type: 'input',
-                name: 'title',
-                message: 'What is the title of your project?',
-            },
-            {
-                type: 'input',
-                name: 'description',
-                message: 'What is the description for your project?',
-            },
-            {
-                type:'input',
-                name: 'table',
-                message: 'What is the table of contents for your project?',
-            },
-            {
-                type: 'input',
-                name: 'installation',
-                message: 'What are the installation instructions for your project?',
-            },
-            {
-                type: 'input',
-                name: 'usage',
-                message: 'How do you use your application?',
-            },
-            {
-                type: 'input',
-                name: 'credits',
-                message: 'Who or what deserves credit for this project?',
-            },
-            {
-                type: 'input',
-                name: 'license',
-                message: 'Add your lisence here.'
-            },
-            {
-                type: 'input',
-                name: 'features',
-                message: 'What features does this project have?'
-            },
-            {
-                type: 'input',
-                name: 'contribute',
-                message: 'How to contribute to this project?'
-            },
-            {
-                type: 'input',
-                name: 'tests',
-                message: 'What tests were done on the project?',
-            },
-            {
-                type: 'input',
-                name: 'github',
-                message: 'What is your GitHub username?',
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: 'What is your email?',
-            },
-            {
-                type: 'input',
-                name: 'contact',
-                message: 'What is the best way to contact you?'
-            }
-        ];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the description for your project?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions for your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How do you use your application?",
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "Who or what deserves credit for this project?",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "Add your lisence here.",
+  },
+  {
+    type: "input",
+    name: "features",
+    message: "What features does this project have?",
+  },
+  {
+    type: "input",
+    name: "contribute",
+    message: "How to contribute to this project?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "What tests were done on the project?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+  {
+    type: "input",
+    name: "contact",
+    message: "What is the best way to contact you?",
+  },
+];
 
-       
 function writeToFile(fileName, data) {
-    const readmeInfo =`
+    const licenseBadge = renderLicenseBadge(data.license);
+    const readmeInfo = `
 # ${data.title}
 
+${licenseBadge}
+    
 ## Description
 ${data.description}
-
-## Table of Contents
-${data.table}
+    
+ ## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
 ## Installation
 ${data.installation}
@@ -90,7 +94,7 @@ ${data.usage}
 ${data.credits}
 
 ## License
-${data.license}
+${renderLicenseSection(data.license)}
 
 ## Badges
 
@@ -108,15 +112,15 @@ ${data.github}
 ${data.email}
 ${data.contact}
 `;
-fs.writeFile(fileName, readmeInfo, (err) =>{
-    err ? console.log(err) : console.log("Success")});
+  fs.writeFile(fileName, readmeInfo, (err) => {
+    err ? console.log(err) : console.log("Success");
+  });
 }
 
 function init() {
-    inquirer
-        .prompt(questions)
-        .then((data) =>{
-        writeToFile('README.md', data);
-})}
+  inquirer.prompt(questions).then((data) => {
+    writeToFile("README.md", data);
+  });
+}
 
 init();
